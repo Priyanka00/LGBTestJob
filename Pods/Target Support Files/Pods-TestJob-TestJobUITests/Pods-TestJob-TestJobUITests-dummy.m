24d774cb549423950e4a7bc25e014d4f8edfe0dd
Priyanka00 <sinhapriyanka2008@gmail.com>
2024-03-04 15:30:25 +0530

    Added SwiftyJson and Alamofire Pods

---
+++ Pods/Target Support Files/Pods-TestJob-TestJobUITests/Pods-TestJob-TestJobUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestJob_TestJobUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestJob_TestJobUITests
+@end
---
+++ Pods/Target Support Files/Pods-TestJob/Pods-TestJob-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestJob : NSObject
+@end
+@implementation PodsDummy_Pods_TestJob
+@end
